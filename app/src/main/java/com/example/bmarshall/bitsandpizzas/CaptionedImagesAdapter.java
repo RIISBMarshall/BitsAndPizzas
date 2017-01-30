ff54c911071c07ce88179912119bda7d2eee0f5b
Brian Marshall <bmarshall@RIIS.com>
2017-01-30 11:35:00 -0500

    minor change

--- app/src/main/java/com/example/bmarshall/bitsandpizzas/CaptionedImagesAdapter.java
+++ app/src/main/java/com/example/bmarshall/bitsandpizzas/CaptionedImagesAdapter.java
@@ -1,7 +1,6 @@
 package com.example.bmarshall.bitsandpizzas;
 
 import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
